a080e27729a7f827728af1b6e524387641d232c1
Filip Przybycien <fprzybycien@gmail.com>
2020-09-22 15:53:30 +0200

    fix async namescheme

--- lada/email.py
+++ lada/email.py
@@ -4,7 +4,7 @@ from flask_mail import Message
 from lada import mail
 
 
-def send_async_email(app, msg):
+def send_sync_email(app, msg):
   with app.app_context():
     mail.send(msg)
 
@@ -12,4 +12,4 @@ def send_email(subject, sender, recipients, text_body, html_body):
   msg = Message(subject, sender=sender, recipients=recipients)
   msg.body = text_body
   msg.html = html_body
-  Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()
+  Thread(target=send_sync_email, args=(current_app._get_current_object(), msg)).start()
