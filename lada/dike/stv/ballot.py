921aab111531d7c9b93c356798c8dd100b9bbadd
Bartlomiej Puget <larhard@gmail.com>
2020-09-24 14:38:53 +0200

    Add argument validators to Ballot

--- lada/dike/stv/ballot.py
+++ lada/dike/stv/ballot.py
@@ -1,9 +1,15 @@
 class Ballot():
   def __init__(self, preference = list(), reject = set()):
-    self.preference = preference
-    self.reject = reject
+    self.preference = list(preference)
+    self.reject = set(reject)
     self.value = 1
     self.check_active()
+
+    if len(set(self.preference) & self.reject):
+      raise ValueError(f'Ballot preference and reject has non-empty intersection: {self}')
+
+    if len(set(self.preference)) != len(self.preference):
+      raise ValueError(f'Ballot preference contains duplicates: {self}')
     
   def check_active(self):
     if len(self.preference) is 0:
