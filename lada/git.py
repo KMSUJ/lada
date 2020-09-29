9195d2d58beb35b6350db716183fc9c52a3acce8
Bartlomiej Puget <larhard@gmail.com>
2020-09-29 18:49:25 +0200

    Fix git path

--- lada/git.py
+++ lada/git.py
@@ -3,7 +3,7 @@ import subprocess
 
 def get_revision_hash():
     try:
-        result = subprocess.check_output(["git", "rev-parse", "HEAD"])
+        result = subprocess.check_output(["/usr/bin/git", "rev-parse", "HEAD"])
         result = result.decode()
         result = result.strip()
         return result
